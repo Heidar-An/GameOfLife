2be74e0f323ce0a67632837bbca5b432f35acec7
Heidar-An <74025356+Heidar-An@users.noreply.github.com>
2021-04-08 18:18:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,47 +15,7 @@ green = (0, 128, 0)
 
 pg.init()
 window = pg.display.set_mode(screen)
-
 squares = [[random.randint(0, 1) for i in range(width // gap)] for j in range(height // gap)]
-
-
-"""
-class game():
-    def __init__(self, width, height):
-        window.fill(white)
-        for i in range(width // gap):
-            for j in range(height // gap):
-                if squares[i][j] == 1:
-                    pg.draw.rect(window, black, Rect(i * gap, j * gap, gap, gap))
-        pg.display.update()
-
-    def repeat(self):
-        next = [["" for i in range(width // gap)] for j in range(height // gap)]
-        for i in range(width // gap):
-            for j in range(height // gap):
-                self.new(i, j, next)
-        squares = next
-        
-
-    def new(self, x, y, next):
-        sum = 0
-        for i in range(-1, 2):
-            for j in range(-1, 2):
-                if 0 <= x + i <= width // gap and 0 <= y + j <= height // gap:
-                    sum += squares[x + i][y + j]
-        sum -= squares[x][y]
-        if squares[x][y] == 1 and (sum == 2 or sum == 3):
-            next[x][y] = 1
-        else:
-            next[x][y] = 0
-        if squares[x][y] == 0 and sum == 3:
-            next[x][y] = 1
-        else:
-            next[x][y] = 0
-            
-game(width, height)
-"""
-
 window.fill(white)
 
 
@@ -71,9 +31,7 @@ def start(width, height, squares):
 def repeat(next1):
     for i in range(width // gap):
         for j in range(height // gap):
-            #if not(i == 0 or i == (width // gap) - 1 or j == 0 or j == (height // gap)- 1):
             next1[i][j] = new(i, j, next1)
-    # print("This is next1", next1)
     return next1
 
 
@@ -96,7 +54,5 @@ def new(x, y, next):
 while True:
     window.fill(white)
     next1 = squares
-    # print("this is next2", squares)
-    # pg.display.update()
     start(width, height, next1)
     squares = repeat(next1)
